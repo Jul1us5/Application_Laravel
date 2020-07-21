99aa4347b32be27ae0eb6bcc3020bba3f100ab8c
Julius <Julius>
2020-07-21 21:57:55 +0300

    BANK style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,12 +5,18 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                <div class="color">
-                ??
+                <div class="card-header"><a href="{{route('home')}}"><b>{{ __('Pagrindinis') }}</b></a>
+            
+                <a href="{{route('account.index')}}">Sąrašas</a>
+                <!-- <a href="{{route('account.index')}}">Peržiureti</a> -->
+                <a href="{{route('account.create')}}">Pridėti naują</a>
+                
+            
                 </div>
+        
+                
+                <div class="card-body">
+
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
