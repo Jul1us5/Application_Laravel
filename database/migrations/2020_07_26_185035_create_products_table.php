102b8740af672a3ec0f3438d399271f7f9049e83
Julius <Julius>
2020-07-26 22:05:18 +0300

    Add some mysql tables

---
+++ database/migrations/2020_07_26_185035_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255);
+            $table->string('name', 255);
+            $table->string('about', 255);
+            $table->string('code', 255);
+            $table->string('notice', 255);
+            $table->string('tag', 255);
+            // $table->string('img', 255);
+            // $table->unsignedBigInteger('account_id');
+            // $table->foreign('account_id')->references('id')->on('accounts');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
