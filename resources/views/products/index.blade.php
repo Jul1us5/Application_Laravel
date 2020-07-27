273576eb335452cda4ad52cfb1149767ca744774
Julius <Julius>
2020-07-27 17:00:43 +0300

    some update

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -7,7 +7,6 @@
 @section('content')
 <div class="container">
 
-    @foreach ($products as $product)
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -21,16 +20,26 @@
                             {{ session('status') }}
                         </div>
                     @endif
-                    
-                    @foreach ($product->getImages as $photo)
-                    <img src="{{asset('images/products/'.$photo->photo)}}" style="width: 250px; height: auto;">
+                    @foreach ($products as $product)
+                        @foreach ($product->getImages as $item)
+                            <div class="product">
+                                <h1>{{$product->title}}</h1>
+                                <img src="{{asset('images/products/'.$item->photo)}}">
+                                
+                                <title>{{$product->name}}</title>
+                                <span>{{$product->about}}</span>
+                                <span>{{$product->code}}</span>
+                                <p>{{$product->notice}}</p>
+                                <span>{{$product->tag}}</span>
+
+                            </div>
+                        @endforeach
                     @endforeach
-                    
+
                 </div>
             </div>
         </div>
     </div>
-    @endforeach
 </div>
 @endsection
 
