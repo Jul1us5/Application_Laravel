84e500c08cce4fba425154f5d7f2c46307b22826
Julius <Julius>
2020-07-27 10:43:57 +0300

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -114,7 +114,7 @@
                         <div class="card-header"><a href="{{route('home')}}"><b>{{ __('Pagrindinis') }}</b></a>
                     
                             <a href="{{route('account.index')}}">Sąrašas</a>
-                            <a href="{{route('product.index')}}">Sukurti</a>
+                            <a href="{{route('product.index')}}">Sukurti..</a>
                             <a href="{{route('account.create')}}">Pridėti naują</a>
                     
                         </div>
